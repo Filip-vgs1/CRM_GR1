52766ab4b347d714e598f84033be5e5ecd3a9fe4
Jakob <jak3007@skole.telemarkfylke.no>
2026-03-10 09:56:32 +0100

    Update meny.php

--- frontend/meny.php
+++ frontend/meny.php
@@ -1,6 +1,7 @@
 <nav>
         <ul>
             <li><a href="../frontend/les_kontakt.php">Hjem</a></li>
+            <li><a href="../frontend/les_custumer.php">Liste over kunder</a></li>
             <li><a href="../backend/contact_create.php">Legg Til Kontaktperson</a></li>
             <li><a href="../backend/customer_create.php">Legg Til Kunde</a></li>
         </ul>
