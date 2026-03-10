0046c0be67adf4d8014b3e0a7d9b2e42ee6f261d
Filip-pvgs <fil5802@skole.telemarkfylke.no>
2026-03-10 10:00:41 +0100

    Update meny.php

--- frontend/meny.php
+++ frontend/meny.php
@@ -7,3 +7,16 @@
         </nav>';
     
 ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../frontend/style.css" type ="text/css">
+    <title>Meny</title>
+</head>
+<body>
+    
+</body>
+</html>
