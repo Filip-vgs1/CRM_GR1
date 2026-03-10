46794920d8f57ad0fa54c65c6a3d840221da26f6
Jakob <jakob@mandelid.net>
2026-03-10 09:07:30 +0100

    Revert "Fix customer creation backend and update UI texts"

--- backend/customer_create_bekreft.php
+++ backend/customer_create_bekreft.php
@@ -9,31 +9,32 @@ if(isset($_GET['ny_customer']) and ($_SERVER['REQUEST_METHOD'] == 'GET')) {
     $firmaAdresse               =   $_GET['firmaAdresse'];
     $firmaPostnr                =   $_GET['firmaPostnr'];
     $firmaTlf                   =   $_GET['firmaTlf'];
-    $firmaStatus                =   'Aktiv';
-    $firmaKundeSiden            =   $_GET['firmaKundeSiden'];
 
+    $firmaStatus = 'Aktiv'
 
     //ser om idfirma finnes fra før
-    $sql = "SELECT * FROM firma WHERE idfirma =:idfirma";
+    $sql = "SELECT * FROM firma WHERE idfirma;
     $stmt = $pdo->prepare($sql);
-    $stmt->bindParam(":idfirma");
+    $stmt->bindParam(":idfirma",$idfirma);
     $stmt->execute();
 
     $dyr = $stmt->fetch(PDO::FETCH_ASSOC);
     
-    if (!$firma){
-        $sql = "INSERT INTO firma (firmaOrganisasjonsnummer, firmaAdresse, firmaPostnr, firmaTlf, firmaStatus, firmaKundeSiden)
-                VALUES (:firmaOrganisasjonsnummer, :firmaAdresse, :firmaPostnr, :firmaTlf, :firmaStatus, :firmaKundeSiden)";
+    print_r($dyr);
+    
+    if (!$dyr){
+        $sql = "INSERT INTO dyrene (kjeledyrNavn, kjeledyrFarge, typeDyr, dyreRase, kjeledyrFodt, kjeledyrHoyde, kjeledyrVekt)
+                VALUES (:kjeledyrNavn, :kjeledyrFarge, :typeDyr, :dyreRase, :kjeledyrFodt, :kjeledyrHoyde, :kjeledyrVekt)";
 
         $stmt = $pdo->prepare($sql);
         // $stmt->bindParam(":idKjeledyr",$idKjeledyr);
-        $stmt->bindParam(":firmaOrganisasjonsnummer",$firmaOrganisasjonsnummer);
-        $stmt->bindParam(":firmaAdresse",$firmaAdresse);
-        $stmt->bindParam(":firmaPostnr",$firmaPostnr);
-        $stmt->bindParam(":firmaTlf",$firmaTlf);
-        $stmt->bindParam(":firmaStatus",$firmaStatus);
-        $stmt->bindParam(":firmaKundeSiden",$firmaKundeSiden);
-        $stmt->bindParam(":ny_custumer",$ny_custumer);
+        $stmt->bindParam(":kjeledyrNavn",$kjeledyrNavn);
+        $stmt->bindParam(":kjeledyrFarge",$kjeledyrFarge);
+        $stmt->bindParam(":typeDyr",$typeDyr);
+        $stmt->bindParam(":dyreRase",$dyreRase);
+        $stmt->bindParam(":kjeledyrFodt",$kjeledyrFodt);
+        $stmt->bindParam(":kjeledyrHoyde",$kjeledyrHoyde);
+        $stmt->bindParam(":kjeledyrVekt",$kjeledyrVekt);
         $stmt->execute();
         }
     else
@@ -55,23 +56,23 @@ else
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="../frontend/style.css" type ="text/css">
-    <title>Registrer ny kunde</title>
+    <title>kontakt</title>
 </head>
 <body>
-    <?php include "../frontend/meny.php"; ?>
+<?php include "meny.php"; ?>
     <header>
-        <p>Registrer ny kunde</p>
+        <p>Registrert ny kontakt</p>
     </header>
 
     <main>
         <?php 
         if ($stmt)
             {
-                echo '<p>En ny kunde er registrert</p>';
+                echo '<p>En ny kontakt er registrert</p>';
             }
             else 
                 {
-                    echo '<p id="slett">Det skjedde en feil, og kunden kunne ikke registreres</p>';
+                    echo '<p id="slett">Det skjedde en feil, og kontakten kunne ikke registreres</p>';
                 }
         ?>
     </main>
