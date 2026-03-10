fa955e87856c8496c3cbfc0e27b0a26a807df2f3
Filip-pvgs <fil5802@skole.telemarkfylke.no>
2026-03-10 10:12:55 +0100

    update meny

--- frontend/meny.php
+++ frontend/meny.php
@@ -1,23 +1,25 @@
-<nav>
-        <ul>
-            <li><a href="../frontend/les_kontakt.php">Hjem</a></li>
-            <li><a href="../frontend/les_custumer.php">Liste over kunder</a></li>
-            <li><a href="../backend/contact_create.php">Legg Til Kontaktperson</a></li>
-            <li><a href="../backend/customer_create.php">Legg Til Kunde</a></li>
-        </ul>
-        </nav>';
-    
-?>
-
 <!DOCTYPE html>
-<html lang="en">
+<html lang="no">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="../frontend/style.css" type ="text/css">
     <title>Meny</title>
+    <link rel="stylesheet" href="../frontend/style.css">
 </head>
+
 <body>
-    
+
+<nav>
+    <ul>
+        <li><a href="../frontend/meny.php">Startside</a></li>
+        <li><a href="../frontend/les_kontakt.php">Liste over kontakter</a></li>
+        <li><a href="../frontend/les_custumer.php">Liste over kunder</a></li>
+        <li><a href="../backend/contact_create.php">Legg Til Kontaktperson</a></li>
+        <li><a href="../backend/customer_create.php">Legg Til Kunde</a></li>
+    </ul>
+</nav>
+
+
+
 </body>
 </html>
