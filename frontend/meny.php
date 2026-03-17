3a0db48d0fcf1cd9de7e17317f9ce4625a925957
Filip-pvgs <fil5802@skole.telemarkfylke.no>
2026-03-17 10:59:19 +0100

    update style

--- frontend/meny.php
+++ frontend/meny.php
@@ -9,7 +9,7 @@
 
 <nav>
         <ul>
-            <li><a href="../index.php">Hjem</a></li>
+            <li><a href="../frontend/index.php">Hjem</a></li>
             <li><a href="../frontend/les_kontakt.php">Liste over kontakter</a></li>
             <li><a href="../frontend/les_custumer.php">Liste over kunder</a></li>
             <li><a href="../backend/contact_create.php">Legg Til Kontaktperson</a></li>
