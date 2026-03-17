618973e367fe52b554f04ed0d7844b44daaccf8b
Jakob <jak3007@skole.telemarkfylke.no>
2026-03-17 11:02:11 +0100

    fiksa index.html til å dukke opp automatisk, og tilpasset nav

--- frontend/meny.php
+++ frontend/meny.php
@@ -1,19 +1,10 @@
-<!DOCTYPE html>
-<html lang="no">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Meny</title>
-    <link rel="stylesheet" href="../frontend/style.css">
-</head>
-
 <nav>
         <ul>
-            <li><a href="../index.php">Hjem</a></li>
-            <li><a href="../frontend/les_kontakt.php">Liste over kontakter</a></li>
-            <li><a href="../frontend/les_custumer.php">Liste over kunder</a></li>
-            <li><a href="../backend/contact_create.php">Legg Til Kontaktperson</a></li>
-            <li><a href="../backend/customer_create.php">Legg Til Kunde</a></li>
-            <li><a href="../backend/about_us.php">Om oss</a></li>
+            <li><a href="../../CRM_GR1/index.php">Hjem</a></li>
+            <li><a href="../../CRM_GR1/frontend/les_kontakt.php">Liste over kontakter</a></li>
+            <li><a href="../../CRM_GR1/frontend/les_custumer.php">Liste over kunder</a></li>
+            <li><a href="../../CRM_GR1/backend/contact_create.php">Legg Til Kontaktperson</a></li>
+            <li><a href="../../CRM_GR1/backend/customer_create.php">Legg Til Kunde</a></li>
+            <li><a href="../../CRM_GR1/backend/about_us.php">Om oss</a></li>
         </ul>
 </nav>
